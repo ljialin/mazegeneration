371b9c4168ccf90c9718efdb33365256f160b561
Jialin Liu <jialin.liu.cn@gmail.com>
2016-12-20 16:00:44 +0000

    update maze txt, add maze paint code

---
+++ src/paint/DrawHumanDesignPacman.java
@@ -0,0 +1,53 @@
+package paint;
+
+import javax.swing.*;
+
+import java.awt.*;
+import java.io.*;
+
+
+/**
+ * Created by Jialin Liu on 20/12/2016.
+ * CSEE, University of Essex, UK
+ * Email: jialin.liu@essex.ac.uk
+ * <p/>
+ * Respect to Google Java Style Guide:
+ * https://google.github.io/styleguide/javaguide.html
+ */
+public class DrawHumanDesignPacman {
+  public static void main(String[] args) {
+    String filename = "./mazes/pacman/level4.txt";
+    int[][] maze = new int[29][26];
+
+    // Reading
+    try{
+      FileReader fr = new FileReader(filename);
+      BufferedReader br = new BufferedReader(fr);
+      String line;
+      int i = 0;
+//      System.out.println(filename);
+      while ((line = br.readLine()) != null){
+        System.out.println(line);
+        int j = 0;
+        for (char ch: line.toCharArray()) {
+          maze[i][j] = (int) (ch - '0');
+          j++;
+        }
+        i++;
+      }
+      br.close();
+//      System.out.println(maze.toString());
+      JFrame frame = new JFrame();
+      MazeDrawer md = new MazeDrawer(maze);
+      frame.add(md);
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setBounds(0,0, (int) (md.getWidth()),(int) (md.getHeight()+md.getWinSizeY()));
+//      frame.setLocationRelativeTo(null);
+      frame.setVisible(true);
+//      Graphics2D g = (Graphics2D) gx;
+    }
+    catch (Exception e){
+      System.out.println(e.toString());
+    }
+  }
+}
--- src/paint/MazeDrawer.java
+++ src/paint/MazeDrawer.java
@@ -1,5 +1,11 @@
 package paint;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.ColorModel;
+
 /**
  * Created by Jialin Liu on 20/12/2016.
  * CSEE, University of Essex, UK
@@ -8,5 +14,69 @@ package paint;
  * Respect to Google Java Style Guide:
  * https://google.github.io/styleguide/javaguide.html
  */
-public class MazeDrawer {
+public class MazeDrawer extends JComponent {
+  int width = 26*20;
+  int height = 29*20;
+  int dimX;
+  int dimY;
+  float winSizeX;
+  float winSizeY;
+  int[][] maze;
+
+
+  public MazeDrawer(int[][] maze) {
+    this.maze = maze;
+    this.dimY = maze.length;
+    this.dimX = maze[0].length;
+
+    winSizeX = this.width/dimX;
+    winSizeY = this.height/dimY;
+
+    System.out.println(dimX + " " + dimY);
+  }
+
+  public int getWidth() {
+    return this.width;
+  }
+
+  public int getHeight() {
+    return this.height;
+  }
+
+  public void paintComponent(Graphics g) {
+    g.setColor(Color.BLUE);
+    g.fillRect(0, 0,(int) (getWidth()+getWinSizeX()),(int) (getHeight()+getWinSizeY()));
+//    for (int i=0; i<this.dimX; i++) {
+//      for (int j = 0; j < this.dimY; j++) {
+//        if(this.maze[j][i]==0) {
+//          g.setColor(Color.BLUE);
+//          g.fillRect((int) (winSizeX*i), (int) (winSizeY*j), (int) (winSizeX), (int) (winSizeY));
+//        }
+//      }
+//    }
+    for (int i=0; i<this.dimX; i++) {
+      for (int j = 0; j < this.dimY; j++) {
+        if(this.maze[j][i]>0) {
+          g.setColor(Color.BLACK);
+          g.fillRect((int) (winSizeX*i-winSizeX*0.5), (int) (winSizeY*j-winSizeY*0.5), (int) (winSizeX*2), (int) (winSizeY*2));
+        }
+      }
+    }
+  }
+
+  public float getWinSizeX() {
+    return winSizeX;
+  }
+
+  public void setWinSizeX(float winSizeX) {
+    this.winSizeX = winSizeX;
+  }
+
+  public float getWinSizeY() {
+    return winSizeY;
+  }
+
+  public void setWinSizeY(float winSizeY) {
+    this.winSizeY = winSizeY;
+  }
 }
