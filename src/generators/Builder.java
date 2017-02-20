f306dd42b7c808acb46bfd30987cc392b8636794
Jialin Liu <jialin.liu.cn@gmail.com>
2017-02-20 17:04:10 +0000

    add builder

--- src/generators/Builder.java
+++ src/generators/Builder.java
@@ -1,28 +1,59 @@
 package generators;
 
-import java.util.ArrayList;
+import tools.IntVector2d;
+
 import java.util.Random;
 
 /**
- * Created by Jialin Liu on 17/01/2017.
+ * Created by Jialin Liu on 20/02/2017.
  * CSEE, University of Essex, UK
  * Email: jialin.liu@essex.ac.uk
  * <p/>
  * Respect to Google Java Style Guide:
  * https://google.github.io/styleguide/javaguide.html
  */
-//http://www.contralogic.com/2d-pac-man-style-maze-generation/
 public class Builder {
-  int x;
-  int y;
-  ArrayList<int[][]> spawners;
-  public Builder(int x, int y) {
-    this.x = x;
-    this.y = y;
-    spawners = new ArrayList<>();
-    int nbSpawners = new Random().nextInt(3)+2;
-    for (int i=0;i<nbSpawners;i++) {
-      spawners.add(n)
+  IntVector2d pos;
+  IntVector2d dir;
+  int dist;
+  int range;
+  Random rdm;
+
+
+  public Builder(IntVector2d pos, IntVector2d dir) {
+    this.pos = new IntVector2d(pos);
+    this.dir = new IntVector2d(dir);
+    this.rdm = new Random();
+    MapBuilder.grid[pos.x][pos.y] = 1;
+  }
+
+  public void randomiseDist() {
+    if (range == 0) {
+      this.dist = rdm.nextInt(5);
+    } else {
+      this.dist = rdm.nextInt(range);
+    }
+  }
+
+  public void expand() {
+    if (this.dist==0) {
+      randomise();
+    }
+    this.pos.add(this.dir);
+    this.dist--;
+  }
+
+  public void randomise() {
+    randomlyRotate();
+    randomiseDist();
+  }
+
+  public void randomlyRotate() {
+    dir.x = 1-Math.abs(dir.x);
+    dir.y = 1-Math.abs(dir.y);
+    if (rdm.nextDouble() < 0.5) {
+      dir.x = -dir.x;
+      dir.y = -dir.y;
     }
   }
 }
